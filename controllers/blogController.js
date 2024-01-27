6369189fca167230b844622e6e351620c19d3b54
JVMergulho <jvitormergulho@gmail.com>
2024-01-27 14:29:48 -0300

    refact: create views/blogs directory

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog')
 const blog_index = (req, res) => {
     Blog.find()
         .then((result) => {
-            res.render('index', {title: "All Blogs", blogs: result})
+            res.render('blogs/index', {title: "All Blogs", blogs: result})
         })
         .catch((err) => {
             console.log(err)
@@ -12,7 +12,7 @@ const blog_index = (req, res) => {
 }
 
 const blog_create_get = (req,res) => {
-    res.render('create',{title: 'New Blog' })
+    res.render('blogs/create',{title: 'New Blog' })
 }
 
 const blog_create_post = (req, res) => {
@@ -20,7 +20,6 @@ const blog_create_post = (req, res) => {
 
     blog.save()
         .then((result) => {
-            console.log("Blog added")
             res.redirect('/blogs')
         })
         .catch((err) => {
@@ -32,7 +31,7 @@ const blog_details = (req, res) => {
     const id = req.params.id
     Blog.findById(id)
             .then(result => {
-                res.render('details', {title: 'Blog Details', blog: result})
+                res.render('blogs/details', {title: 'Blog Details', blog: result})
             })
 }
 
